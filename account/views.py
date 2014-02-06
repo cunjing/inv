70349b2290b82af70df354ece82be01dd15f41a4
cunjing <cunjing222@gmail.com>
2014-02-07 00:31:45 +0800

    add sign in, sign up. modify base html, css.

--- account/views.py
+++ account/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response('index.html')
+
+
+def sign_in(request):
+    return render_to_response('sign_in.html')
+
+
+def sign_up(request):
+    return render_to_response('sign_up.html')
--- inv/urls.py
+++ inv/urls.py
@@ -7,5 +7,7 @@ import account.views
 
 urlpatterns = patterns('',
     url(r'^$', account.views.index),
+    url(r'^sign_in/', account.views.sign_in),
+    url(r'^sign_up/', account.views.sign_up),
     url(r'^admin/', include(admin.site.urls)),
 )
