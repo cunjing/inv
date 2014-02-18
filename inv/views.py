ec42263f61ac3f840f0d37b2de7b41a33336d334
cunjing <cunjing222@gmail.com>
2014-02-18 20:21:18 +0800

    add utf8 head line to all .py files.

--- inv/views.py
+++ inv/views.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 
 
 def index(request):
-    return render_to_response('index.html')
+    return render_to_response('inv/index.html')
